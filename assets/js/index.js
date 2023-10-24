c3440cf8aa52dfe0377f52ed05803212d6678556
Fahim Mahmud <41551321+mdfahimmahmud@users.noreply.github.com>
2023-10-25 02:05:57 +0600

    first changes

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+module.exports = {
+     theme: {
+       extend: {
+         backgroundColor: {
+           'custom-blue': '#0077FF',
+           'custom-green': '#00FF77',
+         },
+       },
+     },
+     variants: {},
+     plugins: [],
+   }
+   
